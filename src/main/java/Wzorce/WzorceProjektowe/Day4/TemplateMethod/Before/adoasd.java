174869b2b7d839fcb198c992741df9c2ea119df9
adriane <adriane@o4.pl>
2022-08-02 19:01:18 +0200

    Zmiany22fa

---
+++ src/main/java/Wzorce/WzorceProjektowe/Day4/TemplateMethod/Before/adoasd.java
@@ -0,0 +1,32 @@
+package Wzorce.WzorceProjektowe.Day4.TemplateMethod.Before;
+
+public class adoasd {
+    public static void main(String[] args) {
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+        System.out.println("SDAD");
+    }
+}
